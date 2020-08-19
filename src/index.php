0528b344b8db2c947785d1aeee869bca300c52ec
Hunter Rancourt <hunter.rancourt@gmail.com>
2020-08-19 11:32:42 -0500

    junk: This is a test of the emergency broadcast system

--- src/index.php
+++ src/index.php
@@ -1,10 +1,11 @@
 <?php
 
-function prettierTestFunction()
-{
+function prettierTestFunction() {
     $var = 'This is using tabs at the time of writting it';
 
     return $var;
 }
+
+$var_test = "Here's another test.";
 
 require './index.html';
